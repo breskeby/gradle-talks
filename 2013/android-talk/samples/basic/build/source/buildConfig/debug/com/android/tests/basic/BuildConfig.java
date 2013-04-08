99a47aac2168c8681275f2ddedbd01677520e4e9
Rene Groeschke <rene@breskeby.com>
2013-04-09 00:20:13 +0200

    add android talk

---
+++ 2013/android-talk/samples/basic/build/source/buildConfig/debug/com/android/tests/basic/BuildConfig.java
@@ -0,0 +1,12 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.android.tests.basic;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+    // lines from default config.
+    private final static boolean DEFAULT = true;
+    private final static String FOO = "foo";
+    // lines from build type: debug
+    private final static boolean DEBUG2 = false;
+}
