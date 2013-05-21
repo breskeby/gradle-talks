8d644807abecaa505c804b3bbc0e95771848d0b7
Rene Groeschke <rene@breskeby.com>
2013-05-22 00:02:42 +0200

    change icons in template; fill in author information.

--- 2013/gradle-summit/slidetemplate/slide_config.js
+++ 2013/gradle-summit/slidetemplate/slide_config.js
@@ -1,15 +1,15 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
-    //eventTitle: 'Google I/O 2013',
+    title: 'Gradle and the <br>Developer Tooling Ecosystem',
+    subtitle: 'An Overview',
+    eventTitle: 'Gradle Summit 2013',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/gradle_favicon_16x16.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -19,12 +19,11 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'René Gröschke',
+    company: 'Principal Engineer, Gradleware',
+    twitter: '@breskeby',
+    www: 'http://www.gradleware.com',
+    github: 'http://github.com/breskeby'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
