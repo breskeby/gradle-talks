2952ec0d38ec26ab9f85512239c4ab9156786884
Rene Groeschke <rene@breskeby.com>
2013-05-30 18:12:18 +0200

    remove samples directory.

--- 2013/android-talk-mfs/samples/multiproject/baseLibrary/build/source/buildConfig/release/com/example/android/multiproject/library/base/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.android.multiproject.library.base;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = false;
-
-}
