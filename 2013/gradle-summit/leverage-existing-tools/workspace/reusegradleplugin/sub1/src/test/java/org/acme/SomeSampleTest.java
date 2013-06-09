27f3ccd82569d3cca711b3d78265a820d6aab9b6
Rene Groeschke <rene@breskeby.com>
2013-06-09 16:33:01 +0200

    add reuseexisting plugin example

---
+++ 2013/gradle-summit/leverage-existing-tools/workspace/reusegradleplugin/sub1/src/test/java/org/acme/SomeSampleTest.java
@@ -0,0 +1,13 @@
+package org.acme;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class SomeSampleTest{
+	
+	@Test 
+	public void testAddition(){
+		assertEquals(2, 1 + 1);
+	}
+}
